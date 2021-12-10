6450e6a1ef4cd1c8ba0bf7f1acd8492c4b9c3e34
taras131 <mossnabitkana@gmail.com>
2021-12-10 17:52:47 +0300

    add component Button

--- src/components/Common/Button/Button.jsx
+++ src/components/Common/Button/Button.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import buttonStyles from './Button.module.scss';
+import classNames from 'classnames';
+import * as PropTypes from 'prop-types';
 
-const Button = () => {
-    return (
-        <div>
-            Button
-        </div>
-    );
+const Button = ({
+  icon = null,
+  alt = '',
+  title,
+  isDisable = false,
+  onClick,
+}) => {
+  return (
+    <button
+      className={classNames(buttonStyles.wrapper, {
+        [buttonStyles.disable]: isDisable,
+        [buttonStyles.active]: !isDisable,
+      })}
+      onClick={onClick}
+      disabled={isDisable}
+    >
+      {icon && <img src={icon} alt={alt} />}
+      <span>{title}</span>
+    </button>
+  );
+};
+Button.propTypes = {
+  icon: PropTypes.string,
+  alt: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  isDisable: PropTypes.bool,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default Button;
