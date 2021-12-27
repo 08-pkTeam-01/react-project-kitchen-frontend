ca246eae2fffab1a46c3545030d3522081c3c600
taras131 <mossnabitkana@gmail.com>
2021-12-27 19:48:12 +0300

    refactor cpmponent header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,28 +1,25 @@
 import React from 'react';
-import {Link, useLocation} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 import headerStyles from './Header.module.scss';
 import {ROUTE_EDITOR, ROUTE_HOME, ROUTE_LOGIN, ROUTE_SETTINGS} from "../../utils/consts";
 import homeIcon from '../../images/icons/home.png'
 import editorIcon from '../../images/icons/editor.png'
 import settingsIcon from '../../images/icons/settings.png'
 import avatarIcon from '../../images/icons/avatar.png'
-import classNames from "classnames";
+import entranceIcon from '../../images/icons/entrance.png'
 
-const MenuItem = ({icon, title, link, pathname}) => {
+const NavItem = ({icon, title, link}) => {
+    const classNames = ({isActive}) => headerStyles.link + ' ' + (isActive && headerStyles.active)
     return (
-        <li className = {classNames(headerStyles.nav_item ,{
-            [headerStyles.active]: pathname === link
-        })}>
-            <Link to={link} className={headerStyles.link}>
+        <li className={headerStyles.nav_item}>
+            <NavLink to={link} className={classNames}>
                 <img src={icon} alt={title}/>
                 <span>{title}</span>
-            </Link>
+            </NavLink>
         </li>
     )
 }
 export const Header = ({appName, currentUser}) => {
-    const {pathname} = useLocation()
-    console.log(pathname)
     return (
         <header className={headerStyles.wrapper}>
             <nav className={headerStyles.content}>
@@ -30,20 +27,16 @@ export const Header = ({appName, currentUser}) => {
                     <h1>{appName.toLowerCase()}</h1>
                 </Link>
                 <ul className={headerStyles.navigation}>
-                    <MenuItem icon={homeIcon} title='Главная' link={ROUTE_HOME} pathname={pathname}/>
+                    <NavItem icon={homeIcon} title='Главная' link={ROUTE_HOME}/>
                     {!currentUser && (
-                        <li>
-                            <Link to={ROUTE_LOGIN} className={headerStyles.link}>
-                                Войти
-                            </Link>
-                        </li>
+                        <NavItem icon={entranceIcon} title='Войти' link={ROUTE_LOGIN}/>
                     )}
                     {currentUser && (
                         <>
-                            <MenuItem icon={editorIcon} title='Новая запись' link={ROUTE_EDITOR} pathname={pathname}/>
-                            <MenuItem icon={settingsIcon} title='Настройки' link={ROUTE_SETTINGS} pathname={pathname}/>
-                            <MenuItem icon={avatarIcon} title={currentUser.username}
-                                      link={`/@${currentUser.username}`} pathname={pathname}/>
+                            <NavItem icon={editorIcon} title='Новая запись' link={ROUTE_EDITOR}/>
+                            <NavItem icon={settingsIcon} title='Настройки' link={ROUTE_SETTINGS}/>
+                            <NavItem icon={avatarIcon} title={currentUser.username}
+                                      link={`/@${currentUser.username}`}/>
                         </>
                     )}
                 </ul>
